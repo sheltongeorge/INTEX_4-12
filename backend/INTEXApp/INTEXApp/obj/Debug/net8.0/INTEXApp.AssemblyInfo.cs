4711f713cd90b9c32a89786741cfaa15014b070a
sheltongeorge <128258667+sheltongeorge@users.noreply.github.com>
2025-04-07 15:11:10 -0600

    Home page included, otherwise same as initial master at start of INTEX

--- backend/INTEXApp/INTEXApp/obj/Debug/net8.0/INTEXApp.AssemblyInfo.cs
+++ backend/INTEXApp/INTEXApp/obj/Debug/net8.0/INTEXApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("INTEXApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2d6ee29c4aa72dcb6f5d1babe36a7fe1e71c01f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7385c476c9772a1fd2820b4341e7200820db680")]
 [assembly: System.Reflection.AssemblyProductAttribute("INTEXApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("INTEXApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecommendationsPage from './pages/RecommendationsPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<RecommendationsPage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/recommendations" element={<RecommendationsPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
