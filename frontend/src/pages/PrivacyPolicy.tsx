c7c43eac1b60cbb72cfb6bde419161dd46ba24b0
parkerfellows <fellowsparker@gmail.com>
2025-04-11 01:14:36 -0600

    privacy

--- frontend/src/pages/PrivacyPolicy.tsx
+++ frontend/src/pages/PrivacyPolicy.tsx
@@ -1,27 +1,24 @@
 import PrivacyPolicy from "../components/gdpr-policy";
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import '../components/gdpr-policy.css';
+import Header from "../components/Header";
 
 function PrivacyPolicyPage() {
-  const navigate = useNavigate();
+ 
 
-  const handleBack = () => {
-    navigate('/');
-  };
+  
 
   return (
+    <>
+    <Header/>
     <div className="privacy-policy-page d-flex flex-column min-vh-100">
-      <header className="d-flex justify-content-between align-items-center p-4">
-        <h1 className="fw-bold">CineNiche</h1>
-        <button className="btn btn-outline-primary" onClick={handleBack}>
-          Back to Home
-        </button>
-      </header>
+      
 
       <main className="flex-grow-1 d-flex flex-column justify-content-center align-items-center text-center px-3">
         <PrivacyPolicy />
       </main>
     </div>
+    </>
   );
 }
 
